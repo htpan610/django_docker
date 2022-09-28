5664a22891eb5630887659a06028509a1217cf0d
htpan610 <htpan1991@163.com>
2022-09-28 08:29:38 +0800

    a

--- django_app/home/urls.py
+++ django_app/home/urls.py
@@ -1,3 +1,12 @@
+import sys
+import os
+import django
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(BASE_DIR)
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_app.settings')
+django.setup()
+
 from django.urls import path
 from . import views
 
